c631733e848ded4affe2b30de5dd7e37bed0995f
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-10 21:01:52 +0530

    Updated Prisma Client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,10 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+
 const prisma = new PrismaClient({
-  
-})
+  datasourceUrl : env.DATABASE_URL,
+}).$extends(withAccelerate())
 const app = new Hono()
 
 app.post('/api/v1/singup', (c) => {
