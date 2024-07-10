5d4fdc2601be4cf894428e21a1f75d70deb268ab
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-10 21:00:31 +0530

    Intialize the Prisma Client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+const prisma = new PrismaClient({
+  
+})
 const app = new Hono()
 
 app.post('/api/v1/singup', (c) => {
