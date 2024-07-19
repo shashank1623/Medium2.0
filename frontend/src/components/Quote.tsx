befaf59ae006a4036e6e8b4da48337d002205ba9
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-19 13:08:23 +0530

    Signup Component Partial Build

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,21 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+            "The Customer support I recived was exceptional. The support team
+            went above and beyond to adress my concerns."
+          </div>
+
+          <div className="max-w-md text-left text-xl font-medium mt-5">
+            Elad Flux
+          </div>
+          <div className="max-w-md text-left text-sm font-light ">
+            CEO | Appwrite
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,19 @@
+import { Quote } from "../components/Quote";
+
+export const Signup = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2">
+
+        <div>
+            A
+        </div>
+
+        <div className="invisible lg:visible">
+            <Quote />
+        </div>
+        
+      </div>
+    </div>
+  );
+};
