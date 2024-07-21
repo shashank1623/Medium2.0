4e27965936230977eb369b558d2eeb98f3ffaa06
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-21 20:29:23 +0530

    Auth Routes States Implemented and Tested at localhost

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
 import { ChangeEvent, useState } from "react";
 import { SignupInput } from "@alias1623/medium-common";
-
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
+    const navigate = useNavigate();
 
     const [postInputs,setPostInputs] = useState<SignupInput>({
         name : "",
         email : "",
         password : ""
     })
+
+    async function sendRequest(){
+        
+        try{
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "singup" : "signin"}`,postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token",jwt);
+            navigate("/blogs")
+
+        }catch(e){
+            //alert the user here that request failed
+            console.log(e);
+        }
+    }
 
   return (
     <div className="h-screen flex justify-center flex-col">
@@ -22,7 +38,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     </div>
                     <div className="text-slate-500">
                         {type === "signin" ? "Don't have an account?" : "Already have an account?" }
-                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                        <Link className="pl-2 underline" to={type === "signin" ? "/singup" : "/signin"}>
                             {type === "signin" ? "Sign up" : "Sign in"}
                         </Link>
                     </div>
@@ -46,7 +62,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                             password: e.target.value
                         })
                     }} />
-                    <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                    <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
         </div>
