7aecc39bdfecec9ecbd8eae62d20f8b38fd7e7e8
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-22 20:18:54 +0530

    Navbar Component Completed and Blogs Page skleton Completed

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,14 +1,34 @@
-import { BlogCard } from "../components/BlogCard"
+import { Appbar } from "../components/Appbar";
+import { BlogCard } from "../components/BlogCard";
 
 export const Blogs = () => {
 
-    return <div>
-        <BlogCard 
-            id = 'c714f30d-6c35-4390-b306-6925003169cc'
-            authorName={"John Doe"} 
-            title={"Google Has Finally Dethroned ChatGPT"} 
-            content={"Google has finally dethroned OpenAI's ChatGPT with their new AI model, LaMDA. LaMDA is a more advanced AI model that can understand context better than ChatGPT. This is a huge win for Google and a big loss for OpenAI."}
+  return (
+    <div>
+      <Appbar />
+
+      <div className="flex justify-center">
+        <div>
+          <BlogCard
+            id="c714f30d-6c35-4390-b306-6925003169cc"
+            authorName={"John Doe"}
+            title={"Google Has Finally Dethroned ChatGPT"}
+            content={
+              "Google has finally dethroned OpenAI's ChatGPT with their new AI model, LaMDA. LaMDA is a more advanced AI model that can understand context better than ChatGPT. This is a huge win for Google and a big loss for OpenAI."
+            }
             publishedDate={"2nd Feb 2024"}
-        />
+          />
+          <BlogCard
+            id="c714f30d-6c35-4390-b306-6925003169cc"
+            authorName={"John Doe"}
+            title={"Google Has Finally Dethroned ChatGPT"}
+            content={
+              "Google has finally dethroned OpenAI's ChatGPT with their new AI model, LaMDA. LaMDA is a more advanced AI model that can understand context better than ChatGPT. This is a huge win for Google and a big loss for OpenAI."
+            }
+            publishedDate={"2nd Feb 2024"}
+          />
+        </div>
+      </div>
     </div>
-}
+  );
+};
