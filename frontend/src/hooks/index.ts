913248be5b36ccac239fa6c4ea07946d27d8942f
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-23 19:12:26 +0530

    Blog Page Completed with state Managment with Fetch Blogs from Backend

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+export interface Blog {
+    "content": string;
+    "title": string;
+    "id": string
+    "author": {
+        "name": string
+    }
+}
+
+export const useBlogs = () => {
+    const [loading, setLoading] = useState(true);
+    const [blogs, setBlogs] = useState<Blog[]>([]);
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+            .then(response => {
+                setBlogs(response.data.post);
+                setLoading(false);
+            })
+    }, [])
+
+    return {
+        loading,
+        blogs
+    }
+}
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,34 +1,29 @@
 import { Appbar } from "../components/Appbar";
 import { BlogCard } from "../components/BlogCard";
+import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
 
-  return (
-    <div>
-      <Appbar />
-
-      <div className="flex justify-center">
-        <div>
-          <BlogCard
-            id="c714f30d-6c35-4390-b306-6925003169cc"
-            authorName={"John Doe"}
-            title={"Google Has Finally Dethroned ChatGPT"}
-            content={
-              "Google has finally dethroned OpenAI's ChatGPT with their new AI model, LaMDA. LaMDA is a more advanced AI model that can understand context better than ChatGPT. This is a huge win for Google and a big loss for OpenAI."
-            }
-            publishedDate={"2nd Feb 2024"}
-          />
-          <BlogCard
-            id="c714f30d-6c35-4390-b306-6925003169cc"
-            authorName={"John Doe"}
-            title={"Google Has Finally Dethroned ChatGPT"}
-            content={
-              "Google has finally dethroned OpenAI's ChatGPT with their new AI model, LaMDA. LaMDA is a more advanced AI model that can understand context better than ChatGPT. This is a huge win for Google and a big loss for OpenAI."
-            }
-            publishedDate={"2nd Feb 2024"}
-          />
-        </div>
-      </div>
+  // store it in state
+  
+  const {loading , blogs} = useBlogs();
+  if(loading){
+    return <div>
+        Loading....
     </div>
-  );
+  }
+  return <div>
+  <Appbar />
+  <div  className="flex justify-center">
+      <div>
+          {blogs.map(blog => <BlogCard
+              id={blog.id}
+              authorName={blog.author.name || "Anonymous"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={"2nd Feb 2024"}
+          />)}
+      </div>
+  </div>
+</div>
 };
