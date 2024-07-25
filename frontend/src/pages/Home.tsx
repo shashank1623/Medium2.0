3add424e38ea214a0d64fd1ec1c4ade0f4a578a2
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-25 22:00:08 +0530

    Home Page Redesigned

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Appbar } from "../components/Appbar"
+import { SpotlightPreview } from "../components/SpotlightPreview"
 
 
 
@@ -6,13 +7,15 @@ export const Home = () => {
 
   return <div>
     <Appbar/>
-
-    <div className="flex justify-center">
+    <SpotlightPreview/>
+    {/* <div className="flex justify-center">
       <div>
         <h1 className="text-3xl font-bold">Welcome to the Blog App</h1>
         <p className="mt-4">This is a blog app where you can share your thoughts and ideas with the world. You can also read blogs shared by other people.</p>
       </div>
-    </div>
+    </div> */}
+
+    {/* <Spotlight/> */}
 
   </div>
 }
